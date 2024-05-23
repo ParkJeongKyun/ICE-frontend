bb0d94d4537ca721f7262e02b38d8980cb7afa64
Kyun <jk.park@nuriggum.com>
2024-05-23 16:38:19 +0900

    코드 정리 #11

--- src/components/KakaoMap/styles.tsx
+++ src/components/KakaoMap/styles.tsx
@@ -3,16 +3,10 @@ import styled from 'styled-components';
 // 카카오 맵
 export const IceMapContainer = styled('div')`
   width: 100%;
-  height: var(--container-img-height-size);
-
-  padding: var(--main-big-padding-size);
-
-  border-radius: var(--container-radius-size);
+  height: 300px;
 `;
 
 export const IceMap = styled('div')`
   width: 100%;
   height: 100%;
-
-  border-radius: var(--container-radius-size);
 `;
