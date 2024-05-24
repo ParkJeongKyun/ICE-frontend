a54125a4e7fa8694ee820e2651377ee72f123bea
Kyun <jk.park@nuriggum.com>
2024-05-24 15:34:23 +0900

    코드 정리 및 마크다운 뷰어 추가 #4

--- src/components/markdown/AboutMD/index.tsx
+++ src/components/markdown/AboutMD/index.tsx
@@ -14,6 +14,16 @@ const txt = `
   - 사이트 디자인 웹앱 형식으로 리뉴얼
   - HexViewer 추가
 
+## 기술 스택
+  - Typescript
+  - Javascript
+  - Go
+  - React
+  - WASM
+
+## 소스 코드
+  - [Github](https://github.com/ParkJeongKyun/ICE-frontend)
+
 ## 업데이트 예정
   - 디자인 개편
   - HexViewer UI 수정
@@ -21,6 +31,8 @@ const txt = `
   - 저장 기능 추가
   - 터미널 기능 추가
   - 개발자 정보 페이지 추가
+  - 다국어 변역 기능 추가
+  - 모바일 디자인 추가
 `;
 
 const AboutMD: React.FC = () => {
