d398b3ea8b24d81a84d416688a89998a92d2fd2f
Kyun <jk.park@nuriggum.com>
2024-05-24 15:11:27 +0900

    코드 정리 및 마크다운 뷰어 추가 #2

--- src/components/markdown/AboutMD/index.tsx
+++ src/components/markdown/AboutMD/index.tsx
@@ -4,9 +4,9 @@ const txt = `
 
 ## 릴리즈 노트
 
-\`\`\`bash
-cd 1234
-\`\`\`
+### v2024.05.24-demo
+  - 사이트 디자인 웹앱 형식으로 리뉴얼
+  - HexViewer 추가
 `;
 
 const AboutMD: React.FC = () => {
--- src/components/markdown/index.tsx
+++ src/components/markdown/index.tsx
@@ -8,17 +8,73 @@ export interface Props {
 
 const ICEMarkDown: React.FC<Props> = ({ markdownText }) => {
   return (
-    <>
+    <MarkdownContainer>
       <MarkDownDiv>
         <Markdown>{markdownText}</Markdown>
       </MarkDownDiv>
-    </>
+    </MarkdownContainer>
   );
 };
 
 export default ICEMarkDown;
 
-export const MarkDownDiv = styled.div`
-  min-width: 400px;
+const MarkdownContainer = styled.div`
+  min-width: 600px;
+  max-height: 400px;
+  overflow-y: auto;
   text-align: left;
+  padding: 10px;
+  margin: 0 auto;
+  border: 1px solid var(--main-line-color);
+  border-radius: 4px;
+`;
+
+export const MarkDownDiv = styled.div`
+  font-size: 12px;
+  line-height: 1.6;
+
+  /* Styling for headings */
+  h1 {
+    font-size: 18px;
+    margin-top: 10px;
+    border-bottom: 1.5px solid var(--main-line-color);
+  }
+
+  h2 {
+    font-size: 16px;
+    margin-top: 10px;
+    border-bottom: 1.5px solid var(--main-line-color);
+  }
+
+  h3 {
+    font-size: 14px;
+    margin-top: 10px;
+    border-bottom: 1.5px solid var(--main-line-color);
+  }
+
+  /* Styling for links */
+  a {
+    color: var(--ice-main-color);
+    text-decoration: underline;
+  }
+
+  /* Styling for code blocks */
+  pre {
+    background-color: var(--main-hover-color);
+    padding: 5px;
+    border-radius: 5px;
+    overflow-x: auto;
+  }
+
+  code {
+    font-family: 'Courier New', Courier, monospace;
+    font-size: 10px;
+  }
+
+  /* Styling for lists */
+  ul,
+  ol {
+    margin-left: 2px;
+    margin-bottom: 5px;
+  }
 `;
