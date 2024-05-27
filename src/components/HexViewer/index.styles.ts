4d2371f0526684316ee12ac9a0a7d75a23f32031
Kyun <jk.park@nuriggum.com>
2024-05-27 15:52:31 +0900

    헥스뷰어 드래그 이벤트 추가 #1

--- src/components/HexViewer/index.styles.ts
+++ src/components/HexViewer/index.styles.ts
@@ -4,6 +4,8 @@ import styled from 'styled-components';
 export const ListDiv = styled(List)`
   padding-top: 10px;
   padding-bottom: 10px;
+  /* 선택 비활성화 */
+  user-select: none;
 `;
 
 export const Row = styled.div`
@@ -28,26 +30,32 @@ export const TextCell = styled.div`
   width: 160px;
 `;
 
-export const HexByte = styled.span`
+export const HexByte = styled.span<{ $selected: boolean }>`
+  cursor: text;
   display: inline-block;
   width: 2ch;
   margin: 2px;
   text-align: center;
-  &.hovered {
-    background-color: var(--main-hover-color);
-  }
+
+  /* 선택된 셀 */
+  ${(props) =>
+    props.$selected &&
+    `
+      background-color: var(--main-hover-color);
+      color: var(--ice-main-color)`}
 `;
 
-export const TextByte = styled.span`
+export const TextByte = styled.span<{ $isDot: boolean; $selected: boolean }>`
+  cursor: text;
   display: inline-block;
   width: 1ch;
   margin: 1px;
   text-align: center;
-  color: var(--ice-main-color);
-  &.hovered {
-    background-color: var(--main-hover-color);
-  }
-  &.isDot {
-    color: var(--main-line-color);
-  }
+  /* 선택된 셀 */
+  ${(props) => props.$selected && 'background-color: var(--main-hover-color);'}
+  /* 점인 경우 */
+  ${(props) =>
+    props.$isDot
+      ? 'color: var(--main-line-color);'
+      : 'color: var(--ice-main-color);'}
 `;
--- src/components/HexViewer/index.tsx
+++ src/components/HexViewer/index.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState, useCallback } from 'react';
 import AutoSizer from 'react-virtualized-auto-sizer';
 import {
   HexByte,
@@ -26,16 +26,42 @@ const byteToChar = (byte: number): string => {
 };
 
 const HexViewer: React.FC<Props> = ({ arrayBuffer }) => {
+  // 드래그 관련 변수
+  const [isDragging, setIsDragging] = useState(false);
+  const [startByteIndex, setStartByteIndex] = useState<number | null>(null);
+  const [endByteIndex, setEndByteIndex] = useState<number | null>(null);
+
+  // 분할용 변수
   const bytesPerRow = 16;
   const buffer = useMemo(() => new Uint8Array(arrayBuffer), [arrayBuffer]);
   const rowCount = Math.ceil(buffer.length / bytesPerRow);
+
+  // 마우스 이벤트
+  const handleMouseDown = useCallback((byteIndex: number) => {
+    setIsDragging(true);
+    setStartByteIndex(byteIndex);
+    setEndByteIndex(byteIndex);
+  }, []);
+
+  const handleMouseMove = useCallback(
+    (byteIndex: number) => {
+      if (isDragging) {
+        setEndByteIndex(byteIndex);
+      }
+    },
+    [isDragging]
+  );
+
+  const handleMouseUp = useCallback(() => {
+    setIsDragging(false);
+  }, []);
 
   const getRowData = ({ index }: { index: number }) => {
     const start = index * bytesPerRow;
     const end = Math.min(start + bytesPerRow, buffer.length);
     const bytes = buffer.slice(start, end);
     const offset = start.toString(16).padStart(8, '0').toUpperCase();
-    return { offset, bytes };
+    return { offset, bytes, start };
   };
 
   const rowRenderer = ({
@@ -47,16 +73,38 @@ const HexViewer: React.FC<Props> = ({ arrayBuffer }) => {
     key: string;
     style: React.CSSProperties;
   }) => {
-    const { offset, bytes } = getRowData({ index });
+    const { offset, bytes, start } = getRowData({ index });
 
     const hexRow: JSX.Element[] = [];
     const textRow: JSX.Element[] = [];
 
     bytes.forEach((byte: number, i: number) => {
-      hexRow.push(<HexByte key={i}>{byteToHex(byte)}</HexByte>);
+      const byteIndex = start + i;
+      const selected =
+        isDragging &&
+        byteIndex >= Math.min(startByteIndex!, endByteIndex!) &&
+        byteIndex <= Math.max(startByteIndex!, endByteIndex!);
+      hexRow.push(
+        <HexByte
+          key={i}
+          $selected={selected}
+          onMouseDown={() => handleMouseDown(byteIndex)}
+          onMouseEnter={() => handleMouseMove(byteIndex)}
+          onMouseUp={handleMouseUp}
+        >
+          {byteToHex(byte)}
+        </HexByte>
+      );
       const str = byteToChar(byte);
       textRow.push(
-        <TextByte key={i} className={`${str == '.' && 'isDot'}`}>
+        <TextByte
+          key={i}
+          $isDot={str == '.'}
+          $selected={selected}
+          onMouseDown={() => handleMouseDown(byteIndex)}
+          onMouseEnter={() => handleMouseMove(byteIndex)}
+          onMouseUp={handleMouseUp}
+        >
           {str}
         </TextByte>
       );
