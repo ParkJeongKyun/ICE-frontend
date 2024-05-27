c8f1eee2f090c76e6e1a0c55ffb50c174e9bc8e9
Kyun <jk.park@nuriggum.com>
2024-05-27 11:19:25 +0900

    about 마크다운 수정 #1

--- src/components/markdown/AboutMD/index.tsx
+++ src/components/markdown/AboutMD/index.tsx
@@ -8,14 +8,36 @@ const txt = `
   - dbzoseh84@gmail.com
   - 디지털 포렌식, 보안, 웹
 
-  [haha](/markdown/relase)
+## [릴리즈 노트](/markdown/relase)
 
+## 기술 스택
+  - Typescript
+  - Javascript
+  - Go
+  - React
+  - WASM
 
 ## 소스 코드
   - [Github](https://github.com/ParkJeongKyun/ICE-frontend)
 
 ## 도움받은 레파지토리
 
+## [업데이트 예정](/markdown/update)
+`;
+
+const relase_txt = `
+## 릴리즈 노트
+
+### v2024.05.07-demo
+  - 레아아웃 조절 기능 추가
+  - 마크다운 기능 업데이트
+
+### v2024.05.24-demo
+  - 사이트 디자인 웹앱 형식으로 리뉴얼
+  - HexViewer 추가
+`;
+
+const update_txt = `
 ## 업데이트 예정
   - 디자인 개편
   - HexViewer UI 수정
@@ -27,28 +49,14 @@ const txt = `
   - 모바일 디자인 추가
 `;
 
-const reltext = `
-## 릴리즈 노트
-
-### v2024.05.24-demo
-  - 사이트 디자인 웹앱 형식으로 리뉴얼
-  - HexViewer 추가
-
-## 기술 스택
-  - Typescript
-  - Javascript
-  - Go
-  - React
-  - WASM
-`;
-
 const AboutMD: React.FC = () => {
   return (
     <>
       <ICEMarkDown
         defaultText={txt}
         childTexts={{
-          relase: reltext,
+          relase: relase_txt,
+          update: update_txt,
         }}
       />
     </>
