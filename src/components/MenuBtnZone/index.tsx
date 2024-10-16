ae5ffe7302558b5cc09f669cf002557ae7460fb9
Kyun <dbzoseh84@gmail.com>
2024-10-16 16:16:18 +0900

    비동기 처리 추가 #1

--- src/components/MenuBtnZone/index.tsx
+++ src/components/MenuBtnZone/index.tsx
@@ -40,125 +40,132 @@ const MenuBtnZone: React.ForwardRefRenderFunction<MenuBtnZoneRef, Props> = (
   const handleAboutClick = () => {
     openModal('about');
   };
+
   // 탭을 추가하고 데이터를 저장하는 함수
   const handleFileChange = async (e: ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (file) {
-      const reader = new FileReader();
-      reader.onload = async (event) => {
-        const arrayBuffer = event.target?.result;
+      const readFileAsArrayBuffer = (file: File): Promise<ArrayBuffer> => {
+        return new Promise((resolve, reject) => {
+          const reader = new FileReader();
+          reader.onload = (event) => {
+            if (event.target?.result instanceof ArrayBuffer) {
+              resolve(event.target.result);
+            } else {
+              reject(new Error('Failed to read file as ArrayBuffer'));
+            }
+          };
+          reader.onerror = (error) => reject(error);
+          reader.readAsArrayBuffer(file);
+        });
+      };
+
+      try {
+        const arrayBuffer = await readFileAsArrayBuffer(file);
         let parsedRows: ExifRow[] | null = null;
         let thumbnail: string = '';
         let lat: string = 'NaN';
         let lng: string = 'NaN';
         let address: string = '';
-        if (arrayBuffer instanceof ArrayBuffer) {
-          const newActiveKey = newTabIndex.current++;
-          const newTab = {
-            label: file.name,
-            children: (
-              <>
-                {arrayBuffer && (
-                  <HexViewer arrayBuffer={arrayBuffer} ref={hexViewerRef} />
-                )}
-              </>
-            ),
-            key: newActiveKey,
-          };
-          setItems((prev) => [...prev, newTab]);
-          setActiveKey(newActiveKey);
 
-          thumbnail = URL.createObjectURL(file);
-          const input_data = new Uint8Array(arrayBuffer);
-          const result = await window.goFunc(input_data);
-          if (result.error) {
-            console.error(result);
-          }
-          if (result.exif_data) {
-            const meta: [
-              {
-                tag: string;
-                comment: string;
-                data: string;
-                origindata: string;
-                type: string;
-                name: string;
-                unit: string;
-                example: any;
-              },
-            ] = JSON.parse(result.exif_data);
+        const newActiveKey = newTabIndex.current++;
+        const newTab = {
+          label: file.name,
+          children: (
+            <>
+              {arrayBuffer && (
+                <HexViewer arrayBuffer={arrayBuffer} ref={hexViewerRef} />
+              )}
+            </>
+          ),
+          key: newActiveKey,
+        };
+        setItems((prev) => [...prev, newTab]);
+        setActiveKey(newActiveKey);
 
-            if (meta) {
-              parsedRows = await Promise.all(
-                meta.map(
-                  async (
-                    {
-                      tag,
-                      comment,
-                      data,
-                      origindata,
-                      name,
-                      type,
-                      unit,
-                      example,
-                    },
-                    index
-                  ) => {
-                    if (tag === 'Location') {
-                      try {
-                        [lat, lng] = origindata
-                          .split(',')
-                          .map((value) => value.trim());
-                        if (isValidLocation(lat, lng)) {
-                          address = await getAddress(lat, lng);
-                          data = address;
-                        }
-                      } catch (error_msg) {
-                        console.log(error_msg);
-                      }
-                    }
-                    return {
-                      id: index + 1,
-                      meta: tag,
-                      comment,
-                      data,
-                      origindata,
-                      name,
-                      type,
-                      unit,
-                      example,
-                    };
-                  }
-                )
-              );
-            }
-          }
-
-          setDatas(
-            (prevDatas) =>
-              new Map(
-                prevDatas.set(newActiveKey, {
-                  fileinfo: {
-                    name: file.name,
-                    lastModified: file.lastModified,
-                    size: file.size,
-                    mime_type: result.mime_type,
-                    extension: result.extension,
-                  },
-                  location: {
-                    lat: lat,
-                    lng: lng,
-                    address: address,
-                  },
-                  thumbnail: thumbnail,
-                  rows: parsedRows,
-                  buffer: arrayBuffer,
-                })
-              )
-          );
+        thumbnail = URL.createObjectURL(file);
+        const input_data = new Uint8Array(arrayBuffer);
+        const result = await window.goFunc(input_data);
+        if (result.error) {
+          console.error(result);
         }
-      };
-      reader.readAsArrayBuffer(file);
+        if (result.exif_data) {
+          const meta: [
+            {
+              tag: string;
+              comment: string;
+              data: string;
+              origindata: string;
+              type: string;
+              name: string;
+              unit: string;
+              example: any;
+            },
+          ] = JSON.parse(result.exif_data);
+
+          if (meta) {
+            parsedRows = await Promise.all(
+              meta.map(
+                async (
+                  { tag, comment, data, origindata, name, type, unit, example },
+                  index
+                ) => {
+                  if (tag === 'Location') {
+                    try {
+                      [lat, lng] = origindata
+                        .split(',')
+                        .map((value) => value.trim());
+                      if (isValidLocation(lat, lng)) {
+                        address = await getAddress(lat, lng);
+                        data = address;
+                      }
+                    } catch (error_msg) {
+                      console.log(error_msg);
+                    }
+                  }
+                  return {
+                    id: index + 1,
+                    meta: tag,
+                    comment,
+                    data,
+                    origindata,
+                    name,
+                    type,
+                    unit,
+                    example,
+                  };
+                }
+              )
+            );
+          }
+        }
+
+        setDatas(
+          (prevDatas) =>
+            new Map(
+              prevDatas.set(newActiveKey, {
+                fileinfo: {
+                  name: file.name,
+                  lastModified: file.lastModified,
+                  size: file.size,
+                  mime_type: result.mime_type,
+                  extension: result.extension,
+                },
+                location: {
+                  lat: lat,
+                  lng: lng,
+                  address: address,
+                },
+                thumbnail: thumbnail,
+                rows: parsedRows,
+                buffer: arrayBuffer,
+              })
+            )
+        );
+      } catch (error) {
+        console.error('Error reading file:', error);
+      }
+
       // 파일 선택 input 초기화
       if (fileInputRef.current) {
         fileInputRef.current.value = ''; // value를 빈 문자열("")로 설정하여 초기화합니다.
