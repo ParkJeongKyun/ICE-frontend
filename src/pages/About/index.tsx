b00a13b71ba3fec036539ac9bc479dc889b48562
kyun <jgpark@aimos.ai>
2025-11-20 16:12:04 +0900

    릴리즈 노트 수정 #1

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -27,13 +27,13 @@ const txt = `
 ## 이메일
   - dbzoseh84@gmail.com
 ## 관심 분야
-  - 디지털 포렌식, 보안, 웹/풀스택 개발
+  - 디지털 포렌식, 보안, 풀스택 개발
 ## 학력
   - 대구초등학교(졸업)
   - 대구제일중학교(졸업)
   - 협성고등학교(졸업)
   - 영진직업전문학교(고교위탁 학생 졸업)
-  - 고려사이버대학교 정보관리보안학과 3학년(재학)
+  - 고려사이버대학교 정보관리보안학과 4학년(재학)
 ## 병역
   - 대한민국 해군 3함대사령부 CERT(정보보호병) 병장 전역
     - 2020년 11월 23일 ~ 2022년 07월 22일
@@ -47,7 +47,7 @@ const txt = `
 ## 기술 스택
   - 언어
     - Python, TypeScript, Javascript/HTML/CSS, Go, Java
-  - Forensic
+  - Digital Forensics
   - Network/System Security
   - Docker, React, NextJS, JSP, FastAPI, Nifi, Jupyter, Kafka, Redis, Superset
 ## 업무 경력
@@ -63,33 +63,27 @@ const txt = `
     - SI, SM, UI/UX, 웹, 파이썬, Java 개발
     - 2024년 07월 15일 ~ 
     - ${calculateExperience(new Date('2024-07'), new Date())}
-## 프로젝트 경력 (SI)
+## 프로젝트 경력
   - AI Link On, Plant AI
     - 고객사 : LG 화학, LG CNS
-    - 근무회사 : NuriggumSoft(주)
     - Python Fast API 백엔드 개발
     - 2022년 07월 ~ 2022년 12월 (${calculateExperience(new Date('2022-07'), new Date('2022-12'))})
   - 차세대 지방 재정 재정분석 시스템
     - 고객사 : 한국지역정보개발원
-    - 근무회사 : NuriggumSoft(주)
     - Jupyter Hub AI 분석 환경 개발, Nifi 데이터 ETL
     - 2023년 01월 ~ 2023년 08월 (${calculateExperience(new Date('2023-01'), new Date('2023-08'))})
   - 현대 ITC 안전 보건 시스템
     - 고객사 : 현대ITC
-    - 근무회사 : NuriggumSoft(주)
     - TypeScript React 웹 프론트엔드 개발
     - 2023년 03월 ~ 2023년 06월 (${calculateExperience(new Date('2023-03'), new Date('2023-06'))})
   - 현대제철 야드비상조업
     - 고객사 : 현대제철, 현대ITC
-    - 근무회사 : NuriggumSoft(주)
     - Java, JavaScript JSP 웹 프론트엔드 개발
     - 2023년 06월 ~ 2023년 12월 (${calculateExperience(new Date('2023-06'), new Date('2023-12'))})
   - 철 스크랩 AIMOS 운영시스템 구축
     - 고객사 : 대한제강, LG CNS, KG ITC
-    - 근무회사 : NuriggumSoft(주)
     - TypeScript React 웹 프론트엔드 개발
     - 2023년 06월 ~ 2024년 04월 (${calculateExperience(new Date('2023-06'), new Date('2024-04'))})
-## 프로젝트 경력 (SM)
   - 철 스크랩 AIMOS 운영시스템 운영 및 개선 작업
     - 고객사 : 대한제강, LG CNS, KG ITC, 아이모스
     - 2024년 07월 ~ (${calculateExperience(new Date('2024-07'), new Date())})
