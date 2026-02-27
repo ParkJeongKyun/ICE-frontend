d6ec30f5e069fffb6dbc5c7c20049fdc49e2c2d3
Kyun <dbzoseh84@gmail.com>
2026-02-27 13:01:29 +0900

    임시 리다이렉트 로직 추가 #2

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,6 +3,23 @@ import NotFoundLayout from '@/layouts/NotFoundLayout/NotFoundLayout';
 export default function NotFound() {
   return (
     <html lang="en">
+      <head>
+        {/* React가 렌더링되기 전, 브라우저가 HTML을 읽자마자 즉시 실행되는 스크립트 */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                var currentPath = window.location.pathname;
+                // 끝에 슬래시가 있으면 즉시 잘라내고 이동
+                if (currentPath.endsWith('/') && currentPath.length > 1) {
+                  var newPath = currentPath.slice(0, -1) + window.location.search + window.location.hash;
+                  window.location.replace(newPath);
+                }
+              })();
+            `,
+          }}
+        />
+      </head>
       <body>
         <NotFoundLayout />
       </body>
