af6680482045abb7454811e9a3e441f6c34db6ee
Kyun <dbzoseh84@gmail.com>
2026-03-07 16:49:00 +0900

    UI/UX 개선#2

--- src/layouts/AboutLayout/AboutLayout.tsx
+++ src/layouts/AboutLayout/AboutLayout.tsx
@@ -13,8 +13,11 @@ import {
   AnimatedBg,
   SimpleListCard,
   SectionBlock,
+  SponsorNote,
+  SponsorNoteSub,
 } from './AboutLayout.styles';
 import Lanyard from './Lanyard/Lanyard';
+import SponsorButton from '@/components/SponsorButton/SponsorButton';
 
 const CURRENT_AGE = new Date().getFullYear() - 2001;
 
@@ -379,6 +382,15 @@ const About: React.FC = () => {
                 </ListItem>
               ))}
             </List>
+          </SimpleListCard>
+        </SectionBlock>
+        <SectionBlock>
+          <SimpleListCard>
+            <SponsorNote>
+              {t('sponsorNote')}
+              <SponsorNoteSub>{t('sponsorSub')}</SponsorNoteSub>
+            </SponsorNote>
+            <SponsorButton />
           </SimpleListCard>
         </SectionBlock>
       </AppContainer>
