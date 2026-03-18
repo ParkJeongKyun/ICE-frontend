bac9520e3f57e2d55c2c21ae5971fd87071820b1
Kyun <dbzoseh84@gmail.com>
2026-03-18 18:16:37 +0900

    데이터 변환기 추가 #1

--- src/layouts/MainLayout/SidePanels/ToolsPanel.tsx
+++ src/layouts/MainLayout/SidePanels/ToolsPanel.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Searcher from '@/components/Searcher/Searcher';
 import DataInspector from '@/components/DataInspector/DataInspector';
 import HashCalculator from '@/components/HashCalculator/HashCalculator';
+import DataConverter from '@/components/DataConverter/DataConverter';
 
 const ToolsPanel: React.FC = () => {
   return (
     <div>
       <Searcher />
       <HashCalculator />
+      <DataConverter />
       <DataInspector />
     </div>
   );
